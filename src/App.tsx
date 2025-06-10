4be1e34624e82b1a85534b937de4acc011d3a81f
Paul Pessoa <paulmspessoa@gmail.com>
2025-06-10 16:04:08 -0300

    ajustando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -3,48 +3,21 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './hooks/useAuth';
 import { Header } from './components/layout/Header';
 import { Footer } from './components/Footer';
-import HomePage from './pages/Index';
-import Login from './pages/Login';
-import Cadastro from './pages/Cadastro';
-import ListagemAgencias from './pages/admin/ListagemAgencias';
-import ModeracaoAgencias from './pages/admin/ModeracaoAgencias';
-import CadastroAgencia from './pages/CadastroAgencia';
-import AgenciasPage from './pages/Agencias';
-import MapaAgencias from './pages/MapaAgencias';
 import { Toaster } from './components/ui/sonner';
-import Analisecurriculo from './pages/Analisecurriculo';
-import Sucesso from './pages/Sucesso';
-import ResultadoCurriculo from './pages/ResultadoCurriculo';
-import CalculadoraRecesso from './pages/CalculadoraRecesso';
-import Admin from './pages/Admin';
-import EsqueciSenha from './pages/EsqueciSenha';
-import NotFound from './pages/NotFound';
 import { ThemeProvider } from './components/theme-provider';
+import { routes } from './route';
 
 const queryClient = new QueryClient();
 
-// Componente que define a estrutura de layout e as rotas
 function AppLayout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-grow pt-14">
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/cadastro" element={<Cadastro />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/admin/agencias" element={<ModeracaoAgencias />} />
-          <Route path="/admin/listagem-agencias" element={<ListagemAgencias />} />
-          <Route path="/cadastro-agencia" element={<CadastroAgencia />} />
-          <Route path="/agencias" element={<AgenciasPage />} />
-          <Route path="/mapa-agencias" element={<MapaAgencias />} />
-          <Route path="/analise-curriculo" element={<Analisecurriculo />} />
-          <Route path="/analise-curriculo/sucesso" element={<Sucesso />} />
-          <Route path="/resultado-curriculo/:id" element={<ResultadoCurriculo />} />
-          <Route path="/calculadora-recesso" element={<CalculadoraRecesso />} />
-          <Route path="/esqueci-senha" element={<EsqueciSenha />} />
-          <Route path="*" element={<NotFound />} />
+          {routes.map(({ path, component: Component }) => (
+            <Route key={path} path={path} element={<Component />} />
+          ))}
         </Routes>
       </main>
       <Footer />
@@ -69,7 +42,6 @@ function App() {
         </BrowserRouter>
       </ThemeProvider>
     </QueryClientProvider>
-
   );
 }
 
---
+++ src/route.ts
@@ -0,0 +1,33 @@
+import HomePage from './pages/Index'
+import Admin from './pages/Admin'
+import Agencias from './pages/Agencias'
+import Analisecurriculo from './pages/Analisecurriculo'
+import Cadastro from './pages/Cadastro'
+import CadastroAgencia from './pages/CadastroAgencia'
+import CalculadoraRecesso from './pages/CalculadoraRecesso'
+import EsqueciSenha from './pages/EsqueciSenha'
+import Login from './pages/Login'
+import MapaAgencias from './pages/MapaAgencias'
+import ResultadoCurriculo from './pages/ResultadoCurriculo'
+import Sucesso from './pages/Sucesso'
+import ListagemAgencias from './pages/admin/ListagemAgencias'
+import ModeracaoAgencias from './pages/admin/ModeracaoAgencias'
+import NotFound from './pages/NotFound'
+
+export const routes = [
+  { path: '/', component: HomePage },
+  { path: '/admin', component: Admin },
+  { path: '/agencias', component: Agencias },
+  { path: '/analise-curriculo', component: Analisecurriculo },
+  { path: '/cadastro', component: Cadastro },
+  { path: '/cadastro-agencia', component: CadastroAgencia },
+  { path: '/calculadora-recesso', component: CalculadoraRecesso },
+  { path: '/esqueci-senha', component: EsqueciSenha },
+  { path: '/login', component: Login },
+  { path: '/mapa-agencias', component: MapaAgencias },
+  { path: '/resultado-curriculo', component: ResultadoCurriculo },
+  { path: '/sucesso', component: Sucesso },
+  { path: '/admin/listagem-agencias', component: ListagemAgencias },
+  { path: '/admin/moderacao-agencias', component: ModeracaoAgencias },
+  { path: '*', component: NotFound },
+]
